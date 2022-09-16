399cf96046b6dc5a5ad06a424da0c7f745a024ec
achrafayada <113729480+achrafayada@users.noreply.github.com>
2022-09-17 00:50:36 +0100

    git push

---
+++ util/serverPath.js
@@ -0,0 +1,3 @@
+exports.serverPath = "https://peaches-2-9af06-default-rtdb.firebaseio.com/"; // example: https://example.example.com/
+
+exports.serverKey = "AAAA-zfjEDc:APA91bF5YAPLOIa0UyShwyaWmMAIsb11HOnc2VW7hOJ9WXIvcMMFL8fN9_R1cvPaHgnmPYY7arosr32IE9Lg2eONCn-rKfkm0KY7jNj0WmObb98fUqUR8VXTnfejBGVCRA8A2KpnGFXQ"; //get this from firebase
